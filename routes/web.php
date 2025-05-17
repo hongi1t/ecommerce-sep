bdd8d6de351db1abf91767c90a83a4b061141faa
Fathiya Aulia <121508888+hongi1t@users.noreply.github.com>
2025-05-17 18:36:26 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,9 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return view('index');
 });
+
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
